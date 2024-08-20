36ff4f7634f15c078740c5169f1e04b896016bca
Sanja <sanjakrsteska@yahoo.com>
2024-08-20 21:01:07 +0200

    Implement UserController with delete, details and index page

---
+++ TravelAgencyApplication.Domain/ViewModel/TAUserIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgencyApplication.Domain.Enum;
+using TravelAgencyApplication.Domain.Identity;
+
+namespace TravelAgencyApplication.Domain.ViewModel
+{
+    public class TAUserIndexViewModel
+    {
+       public List<TAUser> Users {  get; set; }
+       public UserRole CurrentUserRole { get; set; }
+    }
+}
