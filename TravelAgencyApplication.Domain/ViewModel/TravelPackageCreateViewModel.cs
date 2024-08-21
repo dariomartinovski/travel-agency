cf34df3c9d41ee499e69b88b301db8a74f7fac25
Dario <master15820@gmail.com>
2024-08-21 09:12:27 +0200

    Untracked some items

---
+++ TravelAgencyApplication.Domain/ViewModel/TravelPackageCreateViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgencyApplication.Domain.Model;
+
+namespace TravelAgencyApplication.Domain.ViewModel
+{
+    public class TravelPackageCreateViewModel
+    {
+        public TravelPackage TravelPackage { get; set; }
+        public List<Tag> Tags { get; set; }
+        public List<Itinerary> Itineraries { get; set; }
+        public List<DepartureLocation> DepartureLocations { get; set; }
+    }
+
+}
