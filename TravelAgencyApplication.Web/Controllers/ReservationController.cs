8ca3ffe46004cdc9a35cce79e31342184e9c5688
Sanja <sanjakrsteska@yahoo.com>
2024-08-24 11:48:49 +0200

    Add Reservation Controller

--- TravelAgencyApplication.Web/Controllers/ReservationController.cs
+++ TravelAgencyApplication.Web/Controllers/ReservationController.cs
@@ -15,38 +15,39 @@ namespace TravelAgencyApplication.Web.Controllers
             return View();
         }
 
-        public IActionResult PayOrder(string stripeEmail, string stripeToken)
-        {
-            StripeConfiguration.ApiKey = "sk_test_51Io84IHBiOcGzrvu4sxX66rTHq8r5nxIxRiJPbOHB4NwVJOE1jSlxgYe741ITs024uXhtpBFtxm3RoCZc3kafocC00IhvgxkL0";
-            var customerService = new CustomerService();
-            var chargeService = new ChargeService();
-            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        //public IActionResult PayOrder(string stripeEmail, string stripeToken)
+        //{
+        //    StripeConfiguration.ApiKey = "sk_test_51Io84IHBiOcGzrvu4sxX66rTHq8r5nxIxRiJPbOHB4NwVJOE1jSlxgYe741ITs024uXhtpBFtxm3RoCZc3kafocC00IhvgxkL0";
+        //    var customerService = new CustomerService();
+        //    var chargeService = new ChargeService();
+        //    string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-            var order = this._shoppingCartService.getShoppingCartInfo(userId);
+        //    var order = this._shoppingCartService.getShoppingCartInfo(userId);
 
-            var customer = customerService.Create(new CustomerCreateOptions
-            {
-                Email = stripeEmail,
-                Source = stripeToken
-            });
+        //    var customer = customerService.Create(new CustomerCreateOptions
+        //    {
+        //        Email = stripeEmail,
+        //        Source = stripeToken
+        //    });
 
-            var charge = chargeService.Create(new ChargeCreateOptions
-            {
-                Amount = (Convert.ToInt32(order.TotalPrice) * 100),
-                Description = "Travel Agency Application Payment",
-                Currency = "usd",
-                Customer = customer.Id
-            });
+        //    var charge = chargeService.Create(new ChargeCreateOptions
+        //    {
+        //        Amount = (Convert.ToInt32(order.TotalPrice) * 100),
+        //        Description = "Travel Agency Application Payment",
+        //        Currency = "usd",
+        //        Customer = customer.Id
+        //    });
 
-            if (charge.Status == "succeeded")
-            {
-                this.Order();
-                return RedirectToAction("SuccessPayment");
+        //    if (charge.Status == "succeeded")
+        //    {
+        //        this.Order();
+        //        return RedirectToAction("SuccessPayment");
 
-            }
-            else
-            {
-                return RedirectToAction("NotsuccessPayment");
-            }
-        }
+        //    }
+        //    else
+        //    {
+        //        return RedirectToAction("NotsuccessPayment");
+        //    }
+        //}
+    }
 }
